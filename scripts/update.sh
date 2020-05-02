5915f95ff50fcf5b289e6cfb517546a2a2a6cd53
Thomas Cazade <cazade.thomas@gmail.com>
2020-05-02 16:19:30 +0000

    feat(scripts): add echo formatting

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-echo "[poe-currency-monitor-tool] Updating poe-currency-monitor-backend..."
+echo -e "\e[1m\e[36m[poe-currency-monitor-tool] \e[0mUpdating poe-currency-monitor-backend..."
 
 # Create git up alias because it's harmful to use git pull to update a repo,
 # this will add noise to git log in case of branch merging.
 # https://stackoverflow.com/a/17101140
 git config --global alias.up '!git remote update -p; git merge --ff-only @{u}'
 
-echo "[poe-currency-monitor-tool] Updating local repository from remote..."
+echo -e "\e[1m\e[36m[poe-currency-monitor-tool] \e[0mUpdating local repository from remote..."
 git checkout master && git up
 
-echo "[poe-currency-monitor-tool] Running yarn to install dependencies and build"
-yarn install --frozen-lockfile
+echo -e "\e[1m\e[36m[poe-currency-monitor-tool] \e[0mRunning yarn to install dependencies and build..."
+yarn install --frozen-lockfile --silent
 yarn build
 
-echo "[poe-currency-monitor-tool] Restarting systemd process"
+echo -e "\e[1m\e[36m[poe-currency-monitor-tool] \e[0mRestarting systemd process..."
 systemctl restart poecurrencymonitor
 systemctl status poecurrencymonitor
+
+echo -e "\e[1m\e[36m[poe-currency-monitor-tool] \e[0mSuccessfully restarted poecurrencymonitor backend!"
