e64bb3afb91d5eaa605a12739125280b0ac50300
Thomas Cazade <cazade.thomas@gmail.com>
2020-05-26 14:13:15 +0200

    feat: add cache on poe.ninja resources

--- server/poeninja/poeninja.controllers.ts
+++ server/poeninja/poeninja.controllers.ts
@@ -1,8 +1,23 @@
 /* eslint import/prefer-default-export: "off" */
 import { Request, Response } from 'express';
+import Cache from 'node-cache';
 import fetch from 'node-fetch';
 
 import { Currency, Item } from '@interfaces/poeninja.interfaces';
+
+const CACHE_TTL = 60 * 60 * 12;
+
+const cache = new Cache();
+
+/**
+ * Return a cache name based on parameters.
+ *
+ * @param type poe.ninja resource type.
+ * @param league poe.ninja league.
+ */
+function getCacheName(type: string, league: string): string {
+  return `poe-ninja-${type}-${league}`.toLowerCase();
+}
 
 /**
  * Fetch currency-rates from the `/currencyoverview` poe.ninja endpoint, this
@@ -15,6 +30,16 @@ export async function getCurrencyRates(req: Request, res: Response): Promise<voi
   const league = req.query.league as string;
   const language = req.query.language as string;
   const type = req.query.type as string;
+
+  const cacheName = getCacheName(type, league);
+
+  if (cache.has(cacheName)) {
+    const json = cache.get<Currency.Response>(cacheName);
+
+    res.status(200).json({ ...json });
+
+    return Promise.resolve();
+  }
 
   return fetch(`https://poe.ninja/api/data/currencyoverview?league=${league}&type=${type}&language=${language}`, {
     method: 'get',
@@ -29,6 +54,8 @@ export async function getCurrencyRates(req: Request, res: Response): Promise<voi
           error: `Unable to retrieve currency-rates from poe.ninja`,
         });
       } else {
+        cache.set<Currency.Response>(cacheName, json, CACHE_TTL);
+
         res.status(200).json({ ...json });
       }
     });
@@ -46,6 +73,16 @@ export async function getItemRates(req: Request, res: Response): Promise<void> {
   const language = req.query.language as string;
   const type = req.query.type as string;
 
+  const cacheName = getCacheName(type, league);
+
+  if (cache.has(cacheName)) {
+    const json = cache.get<Item.Response>(cacheName);
+
+    res.status(200).json({ ...json });
+
+    return Promise.resolve();
+  }
+
   return fetch(`https://poe.ninja/api/data/itemoverview?league=${league}&type=${type}&language=${language}`, {
     method: 'get',
   })
@@ -59,6 +96,8 @@ export async function getItemRates(req: Request, res: Response): Promise<void> {
           error: `Unable to retrieve item-rates from poe.ninja`,
         });
       } else {
+        cache.set<Item.Response>(cacheName, json, CACHE_TTL);
+
         res.status(200).json({ ...json });
       }
     });
